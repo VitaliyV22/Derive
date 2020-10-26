0b4f1edcbb359df0fb74af11714bebf44c83c1eb
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2020-10-25 22:36:59 -0500

    up to git hub

--- scrape/app.py
+++ scrape/app.py
@@ -8,17 +8,6 @@ import re
 @app.route('/')
 
 def index():
-    
-   headers = {'user-agent' : 'Mozilla/5.0'}
-   page = requests.get("https://www.reddit.com/r/MemeEconomy/", headers=headers)
-   soup = BeautifulSoup(page.content, 'html.parser')
-   imgs = soup.findAll('img', attrs={'alt':'Post image'})
 
-   imglist = []
-   for img in imgs :
-      link_src = img.get('src')
-      imglist.append(link_src)
 
-   picture =imglist[0]
-
-   return render_template("index.html", picture = picture) 
+ return render_template ("index.html")
