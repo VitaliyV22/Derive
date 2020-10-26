56cd28be7a234c588859d37f0c78478d3b7007df
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2020-10-25 23:08:48 -0500

    up to git hub

--- scrape/app.py
+++ scrape/app.py
@@ -9,5 +9,11 @@ import re
 
 def index():
 
+ headers = {'user-agent': 'Mozilla/5.0' }
+ page = requests.get('https://en.wikipedia.org/wiki/Epidemiology_of_depression', headers = headers)
+ soup = BeautifulSoup(page.content, 'html.parser')
+ a = soup.find_all('li')
+ print (a)
+
 
  return render_template ("index.html")
