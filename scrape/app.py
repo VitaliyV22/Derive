abb761bf88e42b6193a21747115a0d538dbf2809
Vitaliy Vitanskyy <vitanskyyv@gmail.com>
2020-10-30 13:27:42 -0500

    second tryb

--- scrape/app.py
+++ scrape/app.py
@@ -8,11 +8,32 @@ from urllib.request import Request, urlopen
 
 @app.route('/')
 
-def sound():
+def index():
  
  
  headers = {'user-agent': 'Mozilla/5.0' }
  page = requests.get('https://encyclotronic.com/synthesizers/korg/prototype-one-r72/', headers = headers)
+ soup = BeautifulSoup(page.content, 'html.parser')
+
+ summary = soup.find('div', class_='ipsGrid_span6')
+
+ print(summary.text)
+
+
+
+
+
+ return render_template ("index.html", summary =summary)
+
+
+
+@app.route('/ms20')
+
+def ms20():
+ 
+ 
+ headers = {'user-agent': 'Mozilla/5.0' }
+ page = requests.get('https://encyclotronic.com/synthesizers/korg/ms-20-r1329/', headers = headers)
  soup = BeautifulSoup(page.content, 'html.parser')
 
  summary = soup.find('div', class_='ipsGrid_span6')
